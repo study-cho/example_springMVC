53783fc11c499687e644d433577598de8f46bcd2
study-cho <dmcho.ex@gmail.com>
2023-04-01 19:38:59 +0900

    기본 구조 설정 - jsp

--- src/main/java/com/example/controller/MainController.java
+++ src/main/java/com/example/controller/MainController.java
@@ -4,9 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
-    @GetMapping("/")
-    public String home() {
-        return "redirect:/main";
+public class MainController {
+
+    @GetMapping("/main")
+    public String main() {
+        return "main";
     }
 }
