3995d998c81b7268edbf948ee0c20b01c2c027f5
study-cho <dmcho.ex@gmail.com>
2023-04-01 21:51:21 +0900

    myBatis 설정

--- src/main/java/com/example/config/ServletAppConfig.java
+++ src/main/java/com/example/config/ServletAppConfig.java
@@ -1,7 +1,15 @@
 package com.example.config;
 
+import com.example.mapper.BoardMapper;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.mapper.MapperFactoryBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
@@ -10,14 +18,55 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.example.controller")
+@PropertySource("/WEB-INF/properties/db.properties")
 public class ServletAppConfig implements WebMvcConfigurer {
+
+    @Value("${db.classname}")
+    private String db_classname;
+
+    @Value("${db.url}")
+    private String db_url;
+
+    @Value("${db.username}")
+    private String db_username;
+
+    @Value("${db.password}")
+    private String db_password;
 
     public void configureViewResolvers(ViewResolverRegistry registry) {
         registry.jsp("/WEB-INF/views/", ".jsp");
     }
 
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**")
-                .addResourceLocations("/resources/");
+        registry.addResourceHandler("/**").addResourceLocations("/resources/");
     }
+
+    //데이터베이스 접속 정보를 관리하는 Bean
+    @Bean
+    public BasicDataSource dataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(db_classname);
+        dataSource.setUsername(db_url);
+        dataSource.setUsername(db_username);
+        dataSource.setPassword(db_password);
+        return dataSource;
+    }
+
+    //쿼리문과 접속 정보 관리하는 객체
+    @Bean
+    public SqlSessionFactory factory(BasicDataSource source) throws Exception {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource());
+        return factoryBean.getObject();
+    }
+
+    //쿼리문 실행을 위한 객체 (Mapper 관리)
+    @Bean
+    public MapperFactoryBean<BoardMapper> getBoardMapper(SqlSessionFactory factory) throws Exception {
+        MapperFactoryBean<BoardMapper> factoryBean = new MapperFactoryBean<>(BoardMapper.class);
+        factoryBean.setSqlSessionFactory(factory);
+        return factoryBean;
+    }
+
+
 }
