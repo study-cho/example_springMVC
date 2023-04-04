5d4fcdd1fc6be0aef4f1de5c5f577e795768d8b0
study-cho <dmcho.ex@gmail.com>
2023-04-04 13:42:20 +0900

    application.properties 오타수정

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -3,8 +3,6 @@ package com.example.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.http.HttpServletRequest;
-
 @Controller
 public class HomeController {
     @GetMapping("/")
