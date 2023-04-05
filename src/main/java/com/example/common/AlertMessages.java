1d413eaeec9f878d93cd466914a42dfe6acd749e
study-cho <dmcho.ex@gmail.com>
2023-04-05 12:48:22 +0900

    AlertMessage 추가

--- src/main/java/com/example/common/AlertMessages.java
+++ src/main/java/com/example/common/AlertMessages.java
@@ -9,9 +9,16 @@ public enum AlertMessages {
     JOIN_SUCCESS("가입이 완료되었습니다"),
     LOGIN_FAIL("로그인에 실패했습니다"),
     LOGIN_SUCCESS("로그인 되었습니다"),
-    MODIFY_SUCCESS("수정되었습니다"),
     LOGOUT("로그아웃 되었습니다"),
-    NOT_LOGIN("로그인해주세요");
+    NOT_LOGIN("로그인해주세요"),
+
+    MODIFY_SUCCESS("수정되었습니다"),
+
+    NOT_WRITER("잘못된 접근입니다"),
+    WRITE_SUCCESS("저장되었습니다"),
+    DELETE("삭제되었습니다")
+
+    ;
 
     private final String message;
 
