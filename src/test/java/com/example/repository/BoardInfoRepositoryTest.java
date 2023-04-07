070d2358849f13978d42826e9024f4542327e7fc
study-cho <dmcho.ex@gmail.com>
2023-04-07 14:22:33 +0900

    boardInfoRepo 테스트 수정

--- src/test/java/com/example/repository/BoardInfoRepositoryTest.java
+++ src/test/java/com/example/repository/BoardInfoRepositoryTest.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,20 +21,37 @@ class BoardInfoRepositoryTest {
     @DisplayName("목록 가져오기")
     void findAll() {
         //given
-        BoardInfo info1 = new BoardInfo(1, "자유게시판");
-        BoardInfo info2 = new BoardInfo(2, "유머게시판");
-        BoardInfo info3 = new BoardInfo(3, "정치게시판");
-        BoardInfo info4 = new BoardInfo(4, "스포츠게시판");
-        boardInfoRepository.save(info1);
-        boardInfoRepository.save(info2);
-        boardInfoRepository.save(info3);
-        boardInfoRepository.save(info4);
+        saveBoardInfo();
 
         //when
         List<BoardInfo> boardInfoList = boardInfoRepository.findAll();
 
         //then
-//        boardInfoList.stream().forEach(c -> System.out.println(c.getBoardInfoName()));
         assertThat(boardInfoList.size()).isEqualTo(4);
+        assertThat(boardInfoList).extracting("boardInfoName")
+                .containsOnly("자유게시판", "유머게시판", "정치게시판", "스포츠게시판");
+    }
+
+    @Test
+    @DisplayName("게시판 이름 가져오기")
+    void getBoardInfoName() {
+        //given
+        saveBoardInfo();
+
+        //when
+        Optional<BoardInfo> findBoardInfo = boardInfoRepository.findById(2);
+
+        //then
+        assertThat(findBoardInfo.get().getBoardInfoName()).isEqualTo("유머게시판");
+
+    }
+
+    private void saveBoardInfo() {
+        List<BoardInfo> list = new ArrayList<>();
+        list.add(new BoardInfo(1, "자유게시판"));
+        list.add(new BoardInfo(2, "유머게시판"));
+        list.add(new BoardInfo(3, "정치게시판"));
+        list.add(new BoardInfo(4, "스포츠게시판"));
+        boardInfoRepository.saveAll(list);
     }
 }
